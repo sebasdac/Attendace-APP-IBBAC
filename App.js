ba019b02fc3b3b193b0c09f9f72a7600662e6794
secot <secoto12@gmail.com>
2025-01-25 13:25:16 -0600

    Version 2.0.0

--- App.js
+++ App.js
@@ -1,40 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabs from './navigation/BottomTabs';
 import BirthdayNotification from './components/BirthdayNotification';
 import * as Updates from 'expo-updates'; // Importar expo-updates para manejar actualizaciones
 
 export default function App() {
-  const [updateAvailable, setUpdateAvailable] = useState(false);
-  const [isUpdateChecked, setIsUpdateChecked] = useState(false);
-
   useEffect(() => {
-    // Verificar si hay una actualización disponible
+    // Verificar y aplicar la actualización automáticamente
     const checkForUpdate = async () => {
       try {
         const update = await Updates.checkForUpdateAsync();
         if (update.isAvailable) {
-          setUpdateAvailable(true);
+          // Descargar la actualización
+          await Updates.fetchUpdateAsync();
+          // Reiniciar la app para aplicar la actualización
+          await Updates.reloadAsync();
         }
-        setIsUpdateChecked(true);  // Marcar que la verificación de actualización se realizó
       } catch (error) {
-        console.error('Error checking for updates:', error);
+        console.error('Error checking or applying updates:', error);
       }
     };
 
-    checkForUpdate();  // Llamar a la función cuando la app se monta
+    checkForUpdate(); // Llamar a la función cuando la app se monta
   }, []);
 
   return (
     <NavigationContainer>
       <BirthdayNotification />
       <BottomTabs />
-      {isUpdateChecked && updateAvailable && (
-        <View style={styles.container}>
-          <Text style={styles.updateText}>¡Hay una actualización disponible!</Text>
-        </View>
-      )}
     </NavigationContainer>
   );
 }
@@ -46,9 +40,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  updateText: {
-    marginTop: 20,
-    fontSize: 16,
-    color: 'red',  // Cambiar el color si es necesario
-  }
 });
